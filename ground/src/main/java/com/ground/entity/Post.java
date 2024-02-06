50fe790e975484850dd5c7a7317ceda3b42f5b1a
dongjun <wkde0258@naver.com>
2024-02-06 11:45:49 +0900

    Ez Project

---
+++ ground/src/main/java/com/ground/entity/Post.java
@@ -0,0 +1,52 @@
+package com.ground.entity;
+
+import java.time.LocalDateTime;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.ground.entity.Post;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private Integer postId;
+
+//	private Integer mh_id;
+
+	private String title;
+	
+	private String cntent;
+
+	private String img_path;
+	
+	private String hash_tag;
+	
+	private String url_path;
+	
+	@CreationTimestamp
+	private LocalDateTime create_date = LocalDateTime.now();
+	
+	@UpdateTimestamp
+	private LocalDateTime update_date = LocalDateTime.now();
+	
+	@ManyToOne
+    @JoinColumn(name = "mh_id") // 외래 키의 이름을 지정
+    private Home home;
+	
+}
